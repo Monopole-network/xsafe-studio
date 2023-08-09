2485f6a79503694efc63ba0cb749733f35a042f0
Matthias Monnier <matthias.monnier@gmail.com>
2023-08-09 09:46:42 +0200

    Update devnet

--- src/config.ts
+++ src/config.ts
@@ -6,10 +6,11 @@ export const walletConnectV2ProjectId = '4f2a173074e230a47805bddfa7ecd1ea';
 export const dAppName = 'xSafe';
 export const decimals = 2;
 export const denomination = 18;
-export const gasPrice = 100_000_000;
+export const gasPrice = 1000000000;
 export const version = 1;
 export const gasPriceModifier = '0.01';
 export const gasPerDataByte = '1500';
+export const chainID = 'D';
 export const gasLimit = 10_000_000;
 export const minGasLimit = 50_000;
 export const maxGasLimit = 1499999999;
@@ -20,7 +21,6 @@ export const walletConnectDeepLink =
 
 export const issueTokenContractAddress =
   'erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqzllls8a5w6u';
-
 const networkSchema = object({
   id: string().defined().required(),
   egldLabel: string().defined().required(),
@@ -33,16 +33,15 @@ const networkSchema = object({
 }).required();
 
 export type NetworkType = InferType<typeof networkSchema>;
-
 export const network: NetworkType = {
-  id: 'testnet',
-  name: 'Testnet',
+  id: 'devnet',
+  name: 'Devnet',
   egldLabel: 'xEGLD',
-  walletAddress: 'https://testnet-wallet.multiversx.com/dapp/init',
-  apiAddress: 'https://testnet-api.multiversx.com',
-  gatewayAddress: 'https://testnet-gateway.multiversx.com',
-  explorerAddress: 'http://testnet-explorer.multiversx.com',
-  storageApi: 'https://testnet-extras-api.multiversx.com',
+  walletAddress: 'https://devnet-wallet.multiversx.com',
+  apiAddress: 'https://devnet-api.multiversx.com',
+  gatewayAddress: 'https://devnet-gateway.multiversx.com',
+  explorerAddress: 'http://devnet-explorer.multiversx.com',
+  storageApi: 'https://devnet-extras-api.multiversx.com',
 };
 
 networkSchema.validate(network, { strict: true }).catch(({ errors }) => {
@@ -50,3 +49,13 @@ networkSchema.validate(network, { strict: true }).catch(({ errors }) => {
 });
 
 export const sampleAuthenticatedDomains = [network.storageApi];
+
+export const relatedBranch = 'develop';
+
+export const xSafeApiUrl = 'https://devnet-api.xsafe.io';
+
+export const xSpotlightContractAddress =
+  'erd1qqqqqqqqqqqqqpgqs8gtvhtt6k7h6khkmudzd6y4z0r08rx4u00svnnxt2';
+
+export const jewelSwapLendingContractAddress =
+  'erd1qqqqqqqqqqqqqpgqk0f3sw6ssqn6e0f39fyrh75k8fh68mds9ckqpw42d6';
